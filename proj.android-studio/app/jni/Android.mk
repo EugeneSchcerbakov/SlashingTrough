5c0cb1802f8ca9b367ae5795da956df0554aae8b
Eugene Shcherbakov <shcherbakov@playrix.com>
2016-02-10 14:26:59 +0300

    Встроена реклама chartboost через sdkbox

--- proj.android-studio/app/jni/Android.mk
+++ proj.android-studio/app/jni/Android.mk
@@ -1,20 +1,27 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(call \
+my-dir)
 
 include $(CLEAR_VARS)
 
 $(call import-add-path,$(LOCAL_PATH)/../../../cocos2d)
 $(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/external)
 $(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/cocos)
+$(call import-add-path, $(LOCAL_PATH))
 
 LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../../Classes/AppDelegate.cpp \
-                   ../../../Classes/HelloWorldScene.cpp
+../../../Classes/AppDelegate.cpp \
+../../../Classes/HelloWorldScene.cpp
 
+LOCAL_CPPFLAGS := -DSDKBOX_ENABLED
+LOCAL_LDLIBS := -landroid \
+-llog
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
+LOCAL_WHOLE_STATIC_LIBRARIES := PluginChartboost \
+sdkbox
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
@@ -28,6 +35,8 @@ LOCAL_STATIC_LIBRARIES := cocos2dx_static
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
+$(call import-module, ./sdkbox)
+$(call import-module, ./pluginchartboost)
 
 # _COCOS_LIB_IMPORT_ANDROID_BEGIN
 # _COCOS_LIB_IMPORT_ANDROID_END
--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -1,73 +1,80 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(call \
+my-dir)
 
 include $(CLEAR_VARS)
 
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+$(call import-add-path, $(LOCAL_PATH))
 
 LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/Abilities.cpp \
-                   ../../Classes/ControlKeyboard.cpp \
-                   ../../Classes/ControlTouch.cpp \
-                   ../../Classes/Cheats.cpp \
-                   ../../Classes/CheatsPanel.cpp \
-                   ../../Classes/DailyTask.cpp \
-                   ../../Classes/DailyMissions.cpp \
-                   ../../Classes/DailyMissionsPopup.cpp \
-                   ../../Classes/EffectsLayer.cpp \
-                   ../../Classes/EffectFlyingIcon.cpp \
-                   ../../Classes/Enemy.cpp \
-                   ../../Classes/EnemyWidget.cpp \
-                   ../../Classes/Field.cpp \
-                   ../../Classes/FieldLayer.cpp \
-                   ../../Classes/FieldSector.cpp \
-                   ../../Classes/FieldLevel.cpp \
-                   ../../Classes/FieldSectorWidget.cpp \
-                   ../../Classes/FieldCamera.cpp \
-                   ../../Classes/GameInfo.cpp \
-                   ../../Classes/GameInterface.cpp \
-                   ../../Classes/GameScene.cpp \
-                   ../../Classes/Hero.cpp \
-                   ../../Classes/HeroAction.cpp \
-                   ../../Classes/HeroWidget.cpp \
-                   ../../Classes/ModelBase.cpp \
-                   ../../Classes/Obstacle.cpp \
-                   ../../Classes/ObstacleWidget.cpp \
-                   ../../Classes/Projectile.cpp \
-                   ../../Classes/ProjectileWidget.cpp \
-                   ../../Classes/PlayerInfo.cpp \
-                   ../../Classes/Store.cpp \
-                   ../../Classes/StartInterface.cpp \
-                   ../../Classes/StatisticsInterface.cpp \
-                   ../../Classes/MissionStartPopup.cpp \
-                   ../../Classes/MapPlayerMark.cpp \
-                   ../../Classes/MapLevelMark.cpp \
-                   ../../Classes/MapWidget.cpp \
-                   ../../Classes/MapInterface.cpp \
-                   ../../Classes/PauseMenu.cpp \
-                   ../../Classes/StoreCategoryButton.cpp \
-                   ../../Classes/StoreInterface.cpp \
-                   ../../Classes/StoreItemButton.cpp \
-                   ../../Classes/StoreItemBaseWidget.cpp \
-                   ../../Classes/StoreWeaponWidget.cpp \
-                   ../../Classes/StoreArmorWidget.cpp \
-                   ../../Classes/StoreCrystallWidget.cpp \
-                   ../../Classes/PopupCore.cpp \
-                   ../../Classes/LevelsCache.cpp \
-                   ../../Classes/ScreenChanger.cpp \
-                   ../../Classes/Utils.cpp \
-                   ../../Classes/Log.cpp \
-                   ../../Classes/VariablesSet.cpp \
-                   ../../Classes/Item.cpp \
-                   ../../Classes/VictoryCondition.cpp
+../../Classes/AppDelegate.cpp \
+../../Classes/Abilities.cpp \
+../../Classes/ControlKeyboard.cpp \
+../../Classes/ControlTouch.cpp \
+../../Classes/Cheats.cpp \
+../../Classes/CheatsPanel.cpp \
+../../Classes/DailyTask.cpp \
+../../Classes/DailyMissions.cpp \
+../../Classes/DailyMissionsPopup.cpp \
+../../Classes/EffectsLayer.cpp \
+../../Classes/EffectFlyingIcon.cpp \
+../../Classes/Enemy.cpp \
+../../Classes/EnemyWidget.cpp \
+../../Classes/Field.cpp \
+../../Classes/FieldLayer.cpp \
+../../Classes/FieldSector.cpp \
+../../Classes/FieldLevel.cpp \
+../../Classes/FieldSectorWidget.cpp \
+../../Classes/FieldCamera.cpp \
+../../Classes/GameInfo.cpp \
+../../Classes/GameInterface.cpp \
+../../Classes/GameScene.cpp \
+../../Classes/Hero.cpp \
+../../Classes/HeroAction.cpp \
+../../Classes/HeroWidget.cpp \
+../../Classes/ModelBase.cpp \
+../../Classes/Obstacle.cpp \
+../../Classes/ObstacleWidget.cpp \
+../../Classes/Projectile.cpp \
+../../Classes/ProjectileWidget.cpp \
+../../Classes/PlayerInfo.cpp \
+../../Classes/Store.cpp \
+../../Classes/StartInterface.cpp \
+../../Classes/StatisticsInterface.cpp \
+../../Classes/MissionStartPopup.cpp \
+../../Classes/MapPlayerMark.cpp \
+../../Classes/MapLevelMark.cpp \
+../../Classes/MapWidget.cpp \
+../../Classes/MapInterface.cpp \
+../../Classes/PauseMenu.cpp \
+../../Classes/StoreCategoryButton.cpp \
+../../Classes/StoreInterface.cpp \
+../../Classes/StoreItemButton.cpp \
+../../Classes/StoreItemBaseWidget.cpp \
+../../Classes/StoreWeaponWidget.cpp \
+../../Classes/StoreArmorWidget.cpp \
+../../Classes/StoreCrystallWidget.cpp \
+../../Classes/PopupCore.cpp \
+../../Classes/LevelsCache.cpp \
+../../Classes/ScreenChanger.cpp \
+../../Classes/Utils.cpp \
+../../Classes/Log.cpp \
+../../Classes/VariablesSet.cpp \
+../../Classes/Item.cpp \
+../../Classes/VictoryCondition.cpp
 
+LOCAL_CPPFLAGS := -DSDKBOX_ENABLED
+LOCAL_LDLIBS := -landroid \
+-llog
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_WHOLE_STATIC_LIBRARIES := PluginChartboost \
+sdkbox
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
@@ -81,6 +88,8 @@ LOCAL_STATIC_LIBRARIES := cocos2dx_static
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
+$(call import-module, ./sdkbox)
+$(call import-module, ./pluginchartboost)
 
 # _COCOS_LIB_IMPORT_ANDROID_BEGIN
 # _COCOS_LIB_IMPORT_ANDROID_END
